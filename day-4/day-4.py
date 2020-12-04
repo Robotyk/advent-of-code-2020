736ad2193505b01401e1d2d4a6deef8165f55e3d
Robotyk <jakub.gizowski@gmail.com>
2020-12-04 19:27:11 +0100

    day 4 solution part 2

--- day-4/day-4.py
+++ day-4/day-4.py
@@ -1,3 +1,5 @@
+import re
+
 def prepare_data():
     file = open("day-4-data", "r")
     data = []
@@ -20,9 +22,19 @@ def prepare_data():
 if __name__ == '__main__':
     data = prepare_data()
 
+    data = [x for x in data if all(key in x.keys() for key in {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"})]
+    print("First part answer: " + str(len(data)))
+
     valid_passports_quantity = 0
+
     for passport in data:
-        if all (key in passport.keys() for key in {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}):
+        if re.match("^(19[2-9]\d|200[0-2])$", passport.get("byr")) \
+                and re.match("^(201[0-9]|2020)$", passport.get("iyr")) \
+                and re.match("^(202[0-9]|2030)$", passport.get("eyr")) \
+                and re.match("^(1[5-8]\dcm|19[0-3]cm|59in|6\din|7[0-6]in)$", passport.get("hgt")) \
+                and re.match("^#([\da-zA-Z]){6}$", passport.get("hcl")) \
+                and re.match("^(amb)|(blu)|(brn)|(gry)|(grn)|(hzl)|(oth)$", passport.get("ecl")) \
+                and re.match("^\d{9}$", passport.get("pid")):
             valid_passports_quantity = valid_passports_quantity + 1
 
-    print("First part answer: " + str(valid_passports_quantity))
+    print("Second part answer: " + str(valid_passports_quantity))
