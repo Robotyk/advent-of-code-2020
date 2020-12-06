b421ef30bfe1f5decb3beef1b0a97e59e1515627
Robotyk <jakub.gizowski@gmail.com>
2020-12-06 07:31:14 +0100

    day 6 solution part 2

--- day-6/day-6.py
+++ day-6/day-6.py
@@ -1,8 +1,11 @@
 if __name__ == '__main__':
     file = open("day-6-data", "r")
+    data = []
+    [data.append(line) for line in file.readlines()]
+
     forms = []
     questions = set()
-    for line in file.readlines():
+    for line in data:
         if line is "\n":
             forms.append(questions)
             questions = set()
@@ -11,7 +14,18 @@ if __name__ == '__main__':
 
     forms.append(questions)
 
-    sum = sum([len(x) for x in forms])
+    print("First part answer: " + str(sum([len(x) for x in forms])))
 
-    print("First part answer: " + str(sum))
+    forms = []
+    for line in data:
+        if line is "\n":
+            forms.append(questions)
+            questions = None
+        elif questions is None:
+            questions = line[:-1]
+        else:
+            questions = [x for x in questions if x in line[:-1]]
 
+    forms.append(questions)
+
+    print("Second part answer: " + str(sum([len(x) for x in forms])))
